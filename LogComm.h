db1c85306a75e8b22a06b7aec416bf1a76b9b084
德州扑克源代码_德州游戏源码_德州扑克源码开发_聊天通讯软件 <develop@deeptexas.ai>
2026-06-24 11:50:56 +0800

    Add files via upload
    servercode

--- LogComm.h
+++ LogComm.h
@@ -2,8 +2,6 @@
 #define __LOGCOMM_H__
 
 #include <util/tc_logger.h>
-#include <sstream>
-#include <vector>
 #include "servant/RemoteLogger.h"
 
 //
@@ -12,66 +10,21 @@ using namespace tars;
 //
 #define ROLLLOG(level) (LOG->level() << "[" << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << "] ")
 #define ROLLLOG_DEBUG (ROLLLOG(debug))
-#define ROLLLOG_INFO (ROLLLOG(info))
-#define ROLLLOG_WARN (ROLLLOG(warn))
+#define ROLLLOG_INFO  (ROLLLOG(info))
+#define ROLLLOG_WARN  (ROLLLOG(warn))
 #define ROLLLOG_ERROR (ROLLLOG(error))
 
 #define FUNC_ENTRY(in) (ROLLLOG(debug) << ">>>> Enter " << __FUNCTION__ << "() in(" << in << ")" << endl)
 #define FUNC_EXIT(out, ret) (ROLLLOG(debug) << "<<<< Exit " << __FUNCTION__ << "() out[" << out << "], ret = " << ret << endl)
 
-#define FDLOG_ERROR (FDLOG("error") << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << "|")
+#define FDLOG_ERROR  (FDLOG("error") << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << "|")
 #define FDLOG_EXCEPT (FDLOG("except") << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << "|")
 
 //配置信息
 #define FDLOG_CONFIG_INFO (FDLOG("config_info") << "|")
 
-//游戏配置数据信息
-#define FDLOG_GAME_CONFIG_INFO (FDLOG("game_config_info") << "|")
+//接口性能边界值
+#define COST_MS 100
 
-// 匿名空间替换static
-namespace
-{
-    // 占位符替换,类似Python
-    //Usage :	auto t = formatString("INSERT INTO {0} WHERE `id` = {1}, `value` = {0}, `name` = {2}", toStringVec(111, 999, "wzc"));
-    std::vector<std::string> subsVec;
-    std::string formatString(std::string target, const std::vector<std::string> &subs)
-    {
-        for (size_t i = 0; i < subs.size(); ++i)
-        {
-            std::string placeholders = "{" + std::to_string(i) + "}";
-            const std::string &sub = subs.at(i);
-            size_t pos = target.find(placeholders);
-            size_t placeholders_len = placeholders.length();
-            while (pos != std::string::npos)
-            {
-                target.replace(pos, placeholders_len, sub);
-                pos = target.find(placeholders);
-            }
-        }
-        return target;
-    }
-
-    template<typename T>
-    std::vector<std::string> toStringVec(T t)
-    {
-        ostringstream os;
-        os << t;
-        subsVec.push_back(os.str());
-        std::vector<std::string> result = subsVec;
-        subsVec.clear();
-        return result;
-    }
-
-    template<typename T, typename... Args>
-    std::vector<std::string> toStringVec(T head, Args... args)
-    {
-        ostringstream os;
-        os << head;
-        subsVec.push_back(os.str());
-        return toStringVec(args...);
-    }
-}
 #endif
-
-
 
